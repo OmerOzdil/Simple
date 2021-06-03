bf27a66a7f1f566b222ad06fe5807c529edc2f8d
OmerOzdil <omerozdil54@gmail.com>
2021-06-03 22:34:07 +0100

    Second Commit

---
+++ Simple/src/Package/Hello2.java
@@ -0,0 +1,10 @@
+package Package;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		System.out.println("Omer Ozdil2");
+
+	}
+
+}
---
+++ Simple/src/Package/hello.java
@@ -0,0 +1,9 @@
+package Package;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("Omer Ozdil");
+	}
+
+}
