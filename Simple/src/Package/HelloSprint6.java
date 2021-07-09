5cd62ea8cd5a98549a332c4a925410665a3ce83d
OmerOzdil <66035535+OmerOzdil@users.noreply.github.com>
2021-07-09 17:16:54 +0100

    Update HelloSprint6.java
    Line has been added form Remote

--- Simple/src/Package/HelloSprint6.java
+++ Simple/src/Package/HelloSprint6.java
@@ -5,6 +5,7 @@ public class HelloSprint6 {
 	public static void main(String[] args) {
 		System.out.println("This branch comes from Sprint 6 work");
 		System.out.println("Extra work has been added");
+		System.out.println("This line has been added from Remote");
 
 	}
 
