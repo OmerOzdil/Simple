5313b482366005da006c8cb6ffdcecb04629b679
OmerOzdil <omerozdil54@gmail.com>
2021-07-09 17:15:02 +0100

    Work has been done

--- Simple/src/Package/HelloSprint6.java
+++ Simple/src/Package/HelloSprint6.java
@@ -4,6 +4,7 @@ public class HelloSprint6 {
 
 	public static void main(String[] args) {
 		System.out.println("This branch comes from Sprint 6 work");
+		System.out.println("Extra work has been added");
 
 	}
 
