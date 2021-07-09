f1c4649a330139d2acb749d05db98226f64d8e4f
OmerOzdil <omerozdil54@gmail.com>
2021-07-09 21:39:00 +0100

    Some work has been done for tikct #123

--- Simple/src/Package/HelloSprint6.java
+++ Simple/src/Package/HelloSprint6.java
@@ -6,6 +6,27 @@ public class HelloSprint6 {
 		System.out.println("This branch comes from Sprint 6 work");
 		System.out.println("Extra work has been added");
 		System.out.println("This line has been added from Remote");
+		
+		
+		String year="2,005 ";
+		
+		// can u parse this year to int?
+		
+		int yearInInt= Integer.parseInt(year.replace(",", "").trim());
+		
+		System.out.println(yearInInt);
+		System.out.println(yearInInt+1);
+		
+		System.out.println(year+1);
+		
+		
+		if(year.equals("2,005 ")) {
+			System.out.println("PASSED");
+		}else {
+			System.out.println("FAILED");
+			
+		}
+		
 
 	}
 
