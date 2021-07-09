0283c2b4e9b60de33e7e470b5b1ae09320ed5d4c
OmerOzdil <omerozdil54@gmail.com>
2021-07-09 21:55:56 +0100

    Line 7 has been added

--- Simple/src/Package/AdemClass.java
+++ Simple/src/Package/AdemClass.java
@@ -4,6 +4,8 @@ public class AdemClass {
 
 	public static void main(String[] args) {
 		System.out.println("This class comes from Adem Branch");
+		System.out.println("More work has been done");
+		
 
 	}
 
